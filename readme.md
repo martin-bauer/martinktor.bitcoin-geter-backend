93dabf87e4bc79fa559320a406cc1ceac337f407
Martin Bauer <mba@adorsys.de>
2021-10-03 21:28:30 +0200

    Add more Info

--- readme.md
+++ readme.md
@@ -9,3 +9,10 @@ To check heroku logs of this app
 
 To build and test locally the heroku step
 >./gradlew stage 
+
+
+localsite for deployment
+> http://0.0.0.0:8080/v1/getcoins
+Heroku Site
+> https://martinktor-cryptobackend.herokuapp.com/v1/getcoins
+> https://api.coinpaprika.com/v1/coins/btc-bitcoin
--- src/main/kotlin/martinktor/routes/GetCoinsRoute.kt
+++ src/main/kotlin/martinktor/routes/GetCoinsRoute.kt
@@ -7,23 +7,13 @@ import io.ktor.routing.*
 import martinktor.data.GetService
 import martinktor.data.model.Coin
 
-private const val BASE_URL = "https://192.168.178.24:8100"
-//private const val BASE_URL ="https://localhost:8100"
 private val client= GetService.get()
-
-private val coins = listOf(
-    Coin("1", "Bitcoin", "50.000"),
-    Coin("1", "Etherum", "4.000"),
-    Coin("1", "Monero", "500"),
-)
-
 
 fun Route.getCoins() {
     get("/v1/getcoins") {
         call.respond(
             HttpStatusCode.OK,
             client.getCoin()
-//            coins.random()
         )
     }
 
