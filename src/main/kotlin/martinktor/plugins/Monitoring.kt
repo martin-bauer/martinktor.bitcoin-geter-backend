35224dccd1ce37da6141946458db6c95db939118
Martin Bauer <mba@adorsys.de>
2021-10-05 00:16:38 +0200

    Add Monitoring/Logging

--- src/main/kotlin/martinktor/plugins/Monitoring.kt
+++ src/main/kotlin/martinktor/plugins/Monitoring.kt
@@ -10,7 +10,16 @@ fun Application.configureMonitoring() {
     
     install(CallLogging) {
         level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
+        filter { call ->
+            call.request.path().startsWith("/")
+        }
+        format { call ->
+            val status = call.response.status()
+            val info = call.request.path()
+            val httpMethod = call.request.httpMethod.value
+            val userAgent = call.request.headers["User-Agent"]
+            "Status: $status, Path: $info, HTTP method: $httpMethod, User agent: $userAgent"
+        }
     }
 
 }
